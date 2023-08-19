b40efb904ae5380cdd417f946b5a94b55320811e
Fernando Moreno Ruiz <illoqpaza2@gmail.com>
2023-08-19 18:34:19 +0200

    Update AppRoutes.jsx

--- imports/routes/AppRoutes.jsx
+++ imports/routes/AppRoutes.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../ui/user/Login";
 import Home from "../ui/pages/Home";
+import Register from "../ui/user/Register"; // Importar el componente Register
+import NotFound from "../ui/pages/NotFound"; // Importar el componente NotFound
 
 export const AppRoutes = () => {
   return (
@@ -9,7 +11,8 @@ export const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="*" element={<div>test not found</div>} />
+        <Route path="/register" element={<Register />} /> {/* Ruta para Register */}
+        <Route path="*" element={<NotFound />} /> {/* Ruta para NotFound */}
       </Routes>
     </BrowserRouter>
   );
