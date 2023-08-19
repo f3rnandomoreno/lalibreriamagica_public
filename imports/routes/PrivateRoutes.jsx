91356ad30bf8594922a36c4330c89453b0285859
fernandomoreno <illoqpaza2@gmail.com>
2023-08-19 14:00:38 +0200

    fix BUG

--- imports/routes/PrivateRoutes.jsx
+++ imports/routes/PrivateRoutes.jsx
@@ -1,26 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Si decides utilizar las páginas y el contexto en el futuro, descomenta las importaciones
 // import the pages
 
-// // import the context
+// import the context
 // import { AuthProvider } from "../context/AuthContext";
 
 export const PrivateRoutes = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <div>test</div>
-        </Route>
-        <Route path="*">
-          <div>test not found</div>
-        </Route>
-        {/* <Route path="/login" component={Login} /> */}
-        {/* <PrivateRoute path="/profile" component={Profile} /> */}
-        {/* <Route path="/register" component={Register} /> */}
-        {/* <Route path="*" component={NotFound} /> */}
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>test</div>} />
+        <Route path="*" element={<div>test not found</div>} />
+        {/* Si decides utilizar estas rutas en el futuro, descomenta las siguientes líneas y asegúrate de importar los componentes correspondientes */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <PrivateRoute path="/profile" element={<Profile />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 };
