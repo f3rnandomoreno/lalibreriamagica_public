54ca36502e0ee6d4c8a0b755d191fd416a168369
fernandomoreno <illoqpaza2@gmail.com>
2023-08-22 19:32:26 +0200

    material ui 5

--- imports/ui/components/commons/Header.jsx
+++ imports/ui/components/commons/Header.jsx
@@ -1,76 +1,41 @@
-import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button, InputBase } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
+import * as React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  InputBase,
+  IconButton,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Header() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography className={classes.title} variant="h6" noWrap>
-            La Librería Mágica
-          </Typography>
-          <div className={classes.search}>
-            <InputBase
-              placeholder="Buscar…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-            <IconButton type="submit" className={classes.iconButton} aria-label="search">
-              <SearchIcon />
-            </IconButton>
-          </div>
-          <Button color="inherit">Inicio</Button>
-          <Button color="inherit">Salas de Lectura</Button>
-          <Button color="inherit">Perfil</Button>
-          <Button color="inherit" variant="outlined">Crear Sala</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          La Librería Mágica
+        </Typography>
+        <InputBase
+          placeholder="Buscar…"
+          sx={{
+            color: "inherit",
+            padding: "0 1rem",
+            width: "20ch",
+            transition: (theme) => theme.transitions.create("width"),
+            "&:focus": { width: "30ch" },
+          }}
+        />
+        <IconButton type="submit" aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <Button color="inherit">Inicio</Button>
+        <Button color="inherit">Salas de Lectura</Button>
+        <Button color="inherit">Perfil</Button>
+        <Button color="inherit" variant="outlined">
+          Crear Sala
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 }
