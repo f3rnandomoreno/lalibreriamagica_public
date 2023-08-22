3552a35fd301f890c2a8d566db0f16558991f406
Fernando Moreno Ruiz <illoqpaza2@gmail.com>
2023-08-22 19:16:11 +0200

    Create Header.jsx

---
+++ imports/ui/components/commons/Header.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Button, InputBase } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            La Librería Mágica
+          </Typography>
+          <div className={classes.search}>
+            <InputBase
+              placeholder="Buscar…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+            <IconButton type="submit" className={classes.iconButton} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </div>
+          <Button color="inherit">Inicio</Button>
+          <Button color="inherit">Salas de Lectura</Button>
+          <Button color="inherit">Perfil</Button>
+          <Button color="inherit" variant="outlined">Crear Sala</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
