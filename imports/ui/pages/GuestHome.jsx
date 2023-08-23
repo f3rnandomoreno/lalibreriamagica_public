bb971a3508bee13d42aac2c97ef52f936cf95561
Fernando Moreno Ruiz <illoqpaza2@gmail.com>
2023-08-24 01:13:16 +0200

    Create GuestHome.jsx

---
+++ imports/ui/pages/GuestHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const GuestHome = () => {
+  return (
+    <div>
+      <h1>Bienvenido a La Librería Mágica</h1>
+      <p>Explora nuestra colección y descubre libros mágicos y fascinantes.</p>
+      <div>
+        {/* Aquí puedes agregar más contenido y enlaces a otras partes de tu sitio para usuarios no autenticados */}
+      </div>
+    </div>
+  );
+};
+
+export default GuestHome;
