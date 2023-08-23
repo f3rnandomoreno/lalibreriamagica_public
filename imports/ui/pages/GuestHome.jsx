38e3b3d7936ac5a53d27d51f79e0e1932471ee24
Fernando Moreno Ruiz <fernandomorenoruiz@gmail.com>
2023-08-24 01:16:55 +0200

    Actualizar GuestHome.jsx

--- imports/ui/pages/GuestHome.jsx
+++ imports/ui/pages/GuestHome.jsx
@@ -1,15 +1,99 @@
-import React from 'react';
+import React from "react";
+import { Container, Button, Typography, Box } from "@mui/material";
+import { styled } from "@mui/system";
+import { useTheme } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
 
-const GuestHome = () => {
+const ImageContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  width: "100%",
+  paddingBottom: "20px",
+});
+
+const Image = styled("img")(({ theme }) => ({
+  width: "60%",
+  maxWidth: "600px",
+  height: "auto",
+  [theme.breakpoints.up("md")]: {
+    width: "30%",
+  },
+}));
+
+const ContentContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "20px",
+});
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  margin: "10px",
+  width: "80%",
+  [theme.breakpoints.up("md")]: {
+    width: "auto",
+  },
+}));
+
+const GuessHome = () => {
+  const theme = useTheme();
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Bienvenido a La Librería Mágica</h1>
-      <p>Explora nuestra colección y descubre libros mágicos y fascinantes.</p>
-      <div>
-        {/* Aquí puedes agregar más contenido y enlaces a otras partes de tu sitio para usuarios no autenticados */}
-      </div>
-    </div>
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "90vh",
+        backgroundColor: "#f0f0f0",
+        padding: "20px",
+        boxSizing: "border-box",
+      }}
+    >
+      <ImageContainer>
+        <Image src="/images/magic_book_home.jpg" alt="Libro mágico" />
+      </ImageContainer>
+
+      <ContentContainer>
+        <Typography variant="h4" component="div" align="center">
+          ¡Bienvenido a la Librería Mágica!
+        </Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            width: "100%",
+            gap: 2,
+            flexWrap: "wrap",
+          }}
+        >
+          <StyledButton
+            variant="contained"
+            onClick={() => {
+              /* Redireccionar a login */
+              navigate("/login");
+            }}
+          >
+            Iniciar sesión
+          </StyledButton>
+
+          <StyledButton
+            variant="contained"
+            onClick={() => {
+              /* Redireccionar a sign up */
+              navigate("/register");
+            }}
+          >
+            Regístrate
+          </StyledButton>
+        </Box>
+      </ContentContainer>
+    </Container>
   );
 };
 
-export default GuestHome;
+export default GuessHome;
