7fdfdb55a9e7d2790a4c4591710ccc9423b13cae
Fernando Moreno Ruiz <illoqpaza2@gmail.com>
2023-08-24 01:11:15 +0200

    Create AuthenticatedHome.jsx

---
+++ imports/ui/pages/AuthenticatedHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AuthenticatedHome = () => {
+  return (
+    <div>
+      <h1>Bienvenido a La Librería Mágica</h1>
+      <p>Has iniciado sesión exitosamente y ahora tienes acceso a características exclusivas.</p>
+      <div>
+        {/* Aquí puedes agregar más contenido y características exclusivas para usuarios autenticados */}
+      </div>
+    </div>
+  );
+};
+
+export default AuthenticatedHome;
