e13b4eba3f08ae1961742f40193dd8dbdd06000c
Fernando Moreno Ruiz <fernandomorenoruiz@gmail.com>
2023-08-24 01:14:35 +0200

    Actualizar AppRoutes.jsx

--- imports/routes/AppRoutes.jsx
+++ imports/routes/AppRoutes.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import Header from '../ui/components/commons/Header';
-import Home from '../ui/pages/Home';
+import AuthenticatedHome from '../ui/pages/AuthenticatedHome'; // Componente para usuarios autenticados
+import GuestHome from '../ui/pages/GuestHome'; // Componente para usuarios no autenticados
 import Login from '../ui/user/Login';
 import Register from '../ui/user/Register';
 import NotFound from '../ui/pages/NotFound';
@@ -15,7 +16,7 @@ export const AppRoutes = () => {
     <BrowserRouter>
       {userIsLoggedIn && <Header />} {/* Mostrar el encabezado solo si el usuario está autenticado */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={userIsLoggedIn ? <AuthenticatedHome /> : <GuestHome />} />
         {!userIsLoggedIn && <Route path="/login" element={<Login />} />}
         {!userIsLoggedIn && <Route path="/register" element={<Register />} />}
         <Route path="*" element={<NotFound />} />
