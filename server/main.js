d1730298ffb6ec3de4b2359893b7f8d506c9f4e6
fernandomoreno <illoqpaza2@gmail.com>
2023-08-24 19:25:39 +0200

    add facebook and google variables

--- server/main.js
+++ server/main.js
@@ -1,1 +1,27 @@
-Meteor.startup(() => {});
+import { ServiceConfiguration } from "meteor/service-configuration";
+
+Meteor.startup(() => {
+  // Configuración para Google
+  ServiceConfiguration.configurations.upsert(
+    { service: "google" },
+    {
+      $set: {
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        secret: process.env.GOOGLE_SECRET,
+        loginStyle: "popup",
+      },
+    }
+  );
+
+  // Configuración para Facebook
+  ServiceConfiguration.configurations.upsert(
+    { service: "facebook" },
+    {
+      $set: {
+        appId: process.env.FACEBOOK_APP_ID,
+        secret: process.env.FACEBOOK_SECRET,
+        loginStyle: "popup",
+      },
+    }
+  );
+});
