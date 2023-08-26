2bf2bb2509d916257c6992c66ded838fec0701db
Fernando Moreno Ruiz <fernandomorenoruiz@gmail.com>
2023-08-26 22:15:14 +0200

    Actualizar AuthenticatedHome.jsx

--- imports/ui/pages/AuthenticatedHome.jsx
+++ imports/ui/pages/AuthenticatedHome.jsx
@@ -1,14 +1,54 @@
 import React from 'react';
+import { Button, Container, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  authenticatedHome: {
+    textAlign: 'center',
+    padding: theme.spacing(3),
+  },
+  introText: {
+    fontSize: '1.2em',
+    margin: theme.spacing(2, 0),
+  },
+  actionSection: {
+    margin: theme.spacing(3, 0),
+  },
+  createRoomBtn: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    padding: theme.spacing(1, 2),
+    borderRadius: theme.shape.borderRadius,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.dark,
+    },
+  },
+  bookCarousel: {
+    // Estilos para el carrusel de libros
+  },
+}));
 
 const AuthenticatedHome = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Bienvenido a La Librería Mágica</h1>
-      <p>Has iniciado sesión exitosamente y ahora tienes acceso a características exclusivas.</p>
-      <div>
-        {/* Aquí puedes agregar más contenido y características exclusivas para usuarios autenticados */}
+    <Container className={classes.authenticatedHome}>
+      <Typography variant="h4" component="h1">
+        Bienvenido a La Librería Mágica 📚✨
+      </Typography>
+      <Typography className={classes.introText}>
+        Descubre un mundo lleno de aventuras, misterios y conocimientos. 
+        ¡Invita a tus amigos y compartan juntos la magia de la lectura!
+      </Typography>
+      <div className={classes.actionSection}>
+        <Button className={classes.createRoomBtn}>
+          Crear Sala de Lectura
+        </Button>
       </div>
-    </div>
+      <div className={classes.bookCarousel}>
+        {/* Aquí puedes agregar un carrusel de portadas de libros al estilo Netflix */}
+      </div>
+    </Container>
   );
 };
 
