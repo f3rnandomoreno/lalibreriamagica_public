fe0c95d03e555ac00bd73a47e88e70032d1e3967
Fernando Moreno Ruiz <fernandomorenoruiz@gmail.com>
2023-08-26 23:27:39 +0200

    using styled

--- imports/ui/pages/AuthenticatedHome.jsx
+++ imports/ui/pages/AuthenticatedHome.jsx
@@ -1,54 +1,48 @@
-import React from 'react';
-import { Button, Container, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import React from "react";
+import { Button, Container, Typography } from "@mui/material";
+import { styled } from "@mui/system";
 
-const useStyles = makeStyles((theme) => ({
-  authenticatedHome: {
-    textAlign: 'center',
-    padding: theme.spacing(3),
-  },
-  introText: {
-    fontSize: '1.2em',
-    margin: theme.spacing(2, 0),
-  },
-  actionSection: {
-    margin: theme.spacing(3, 0),
-  },
-  createRoomBtn: {
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.common.white,
-    padding: theme.spacing(1, 2),
-    borderRadius: theme.shape.borderRadius,
-    '&:hover': {
-      backgroundColor: theme.palette.primary.dark,
-    },
-  },
-  bookCarousel: {
-    // Estilos para el carrusel de libros
-  },
-}));
+const AuthenticatedHomeContainer = styled(Container)`
+  text-align: center;
+  padding: ${(props) => props.theme.spacing(3)}px;
+`;
+
+const IntroText = styled(Typography)`
+  font-size: 1.2em;
+  margin: ${(props) => props.theme.spacing(2, 0)}px;
+`;
+
+const ActionSection = styled("div")`
+  margin: ${(props) => props.theme.spacing(3, 0)}px;
+`;
+
+const CreateRoomBtn = styled(Button)`
+  background-color: ${(props) => props.theme.palette.primary.main};
+  color: ${(props) => props.theme.palette.common.white};
+  padding: ${(props) => props.theme.spacing(1, 2)}px;
+  border-radius: ${(props) => props.theme.shape.borderRadius}px;
+  &:hover {
+    background-color: ${(props) => props.theme.palette.primary.dark};
+  }
+`;
 
 const AuthenticatedHome = () => {
-  const classes = useStyles();
-
   return (
-    <Container className={classes.authenticatedHome}>
+    <AuthenticatedHomeContainer>
       <Typography variant="h4" component="h1">
         Bienvenido a La Librería Mágica 📚✨
       </Typography>
-      <Typography className={classes.introText}>
-        Descubre un mundo lleno de aventuras, misterios y conocimientos. 
-        ¡Invita a tus amigos y compartan juntos la magia de la lectura!
-      </Typography>
-      <div className={classes.actionSection}>
-        <Button className={classes.createRoomBtn}>
-          Crear Sala de Lectura
-        </Button>
-      </div>
-      <div className={classes.bookCarousel}>
+      <IntroText>
+        Descubre un mundo lleno de aventuras, misterios y conocimientos. ¡Invita
+        a tus amigos y compartan juntos la magia de la lectura!
+      </IntroText>
+      <ActionSection>
+        <CreateRoomBtn>Crear Sala de Lectura</CreateRoomBtn>
+      </ActionSection>
+      <div>
         {/* Aquí puedes agregar un carrusel de portadas de libros al estilo Netflix */}
       </div>
-    </Container>
+    </AuthenticatedHomeContainer>
   );
 };
 
